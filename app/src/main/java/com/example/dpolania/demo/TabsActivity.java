a380ab90e85ebb23ec2321206fe067570f093ca4
David Polania <davidpolaniaac@users.noreply.github.com>
2018-09-08 10:39:43 -0500

    new fab button - FloatingActionButton

--- app/src/main/java/com/example/dpolania/demo/TabsActivity.java
+++ app/src/main/java/com/example/dpolania/demo/TabsActivity.java
@@ -1,12 +1,16 @@
 package com.example.dpolania.demo;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class TabsActivity extends AppCompatActivity {
 
     private TabLayout tabLayout;
+    private FloatingActionButton fabBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,14 @@ public class TabsActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
         tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
 
+        fabBtn = (FloatingActionButton) findViewById(R.id.fabBtn);
+
+        fabBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("TabActivity", "FAB Pressed");
+            }
+        });
     }
+
 }
