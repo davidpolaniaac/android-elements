f21abc18520555a81310afef8c526c213fcd241b
David Polania <davidpolaniaac@users.noreply.github.com>
2018-09-08 09:22:58 -0500

    new toolbar

--- app/src/main/java/com/example/dpolania/demo/DisplayMessageActivity.java
+++ app/src/main/java/com/example/dpolania/demo/DisplayMessageActivity.java
@@ -3,20 +3,43 @@ package com.example.dpolania.demo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
-
-
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         ((TextView)findViewById(R.id.textView)).setText(message);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.display_message_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+        }else if(id == R.id.app_bar_delete){
+            ((TextView)findViewById(R.id.textView)).setText("");
+        }else if(id == R.id.app_bar_search){
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
